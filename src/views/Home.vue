01005dc5db1526d16c9a2df1aae9e01fa45b8f3d
ajf214 <alex.j.frankel@gmail.com>
2018-11-27 09:27:34 -0800

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+      <h1>Lawrence Farms Antiques</h1>
+      <img src="fleur-de-lis" />
+
+      <div class="carousel"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  @media screen and (max-width: 1000px) {
+
+  }
+
+</style>
