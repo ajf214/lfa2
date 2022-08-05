559834d27e11fd0d93e94f6214be12559c8256ed
Alex Frankel <alfran@microsoft.com>
2022-08-05 08:51:15 -0400

    add datadog RUM monitoring

--- lfa2/src/main.js
+++ lfa2/src/main.js
@@ -6,7 +6,23 @@ import getEnv from '@/utils/env'
 
 Vue.config.productionTip = false
 
-console.log(getEnv('VUE_APP_BASE_URL'))
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: 'ef963356-0eca-4c60-87a5-ef70de349123',
+    clientToken: 'pubc326e4428d43fe290364451852a3b4a6',
+    site: 'datadoghq.com',
+    service:'lfa2',
+    env:'dev',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    premiumSampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
 
 Vue.mixin({
   // distribute runtime config into every vue component
