28b5555d7e1d66d79bdbca83372009ef7f1650c7
Alex Frankel <alex@Alexs-MacBook-Air.local>
2026-03-27 10:04:24 -0700

    remove datadog

--- lfa2/src/main.js
+++ lfa2/src/main.js
@@ -6,23 +6,6 @@ import getEnv from '@/utils/env'
 
 Vue.config.productionTip = false
 
-import { datadogRum } from '@datadog/browser-rum';
-
-datadogRum.init({
-    applicationId: 'ef963356-0eca-4c60-87a5-ef70de349123',
-    clientToken: 'pubc326e4428d43fe290364451852a3b4a6',
-    site: 'datadoghq.com',
-    service:'lfa2',
-    env: getEnv('VUE_APP_DATADOG_MODE'),
-    // Specify a version number to identify the deployed version of your application in Datadog 
-    // version: '1.0.0', // todo -- pass version here
-    sampleRate: 100,
-    premiumSampleRate: 100,
-    trackInteractions: true,
-    defaultPrivacyLevel:'mask-user-input'
-});
-    
-datadogRum.startSessionReplayRecording();
 
 Vue.mixin({
   // distribute runtime config into every vue component
